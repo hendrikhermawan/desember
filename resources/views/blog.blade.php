1f22e331e1642ba8ab42dad32b2ceef5c542be07
hendrikhermawan <hendrikprima97@gmail.com>
2025-12-31 13:30:41 +0700

    first commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BLog</title>
+</head>
+<body>
+    
+<h1>judul blog</h1>
+<h2>sub bab judul</h2>
+lar
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dicta quidem doloribus, modi quisquam odit quae cumque deserunt blanditiis tempora est quas repellat optio perspiciatis ratione delectus corporis nam accusamus.</p>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// coba bikin view baru dengan nama about
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/', function () {
+    return view('home',['nama' => 'Hendrik Hermawan']);
+});
+
+// buat route baru dengan nama blog dan contact
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+
